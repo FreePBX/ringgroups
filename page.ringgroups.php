2201908a4ae9785e910aa449a9a1eca493921ff2
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-08-21 04:47:57 +0000

    #2193 moh path hardcoded

--- page.ringgroups.php
+++ page.ringgroups.php
@@ -256,7 +256,7 @@ if ($action == 'delGRP') {
 		<td>
 			<select name="ringing"/>
 			<?php
-				$tresults = music_list("/var/lib/asterisk/mohmp3");
+				$tresults = music_list($amp_conf['ASTVARLIBDIR']."/mohmp3");
 				$cur = (isset($ringing) ? $ringing : 'Ring');
 				echo '<option value="Ring">'._("Ring")."</option>";
 				if (isset($tresults[0])) {
