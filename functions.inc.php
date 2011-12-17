c46ccc58579d25b8b17f3c32763f91c55e2cfe95
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2011-12-17 21:02:34 +0000

    adds FREEPBX_IS_AUTH checking to most module files re #5478

--- functions.inc.php
+++ functions.inc.php
@@ -1,4 +1,5 @@
 <?php /* $Id$ */
+if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 
 // The destinations this module provides
 // returns a associative arrays with keys 'destination' and 'description'
