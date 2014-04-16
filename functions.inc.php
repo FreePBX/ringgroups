e7b8eadb256ea68949e2cf454a95da3b754b83cf
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 15:56:27 -0500

    new license tags

--- functions.inc.php
+++ functions.inc.php
@@ -1,6 +1,8 @@
 <?php /* $Id$ */
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2013 Schmooze Com Inc.
+//
 // The destinations this module provides
 // returns a associative arrays with keys 'destination' and 'description'
 function ringgroups_destinations() {
