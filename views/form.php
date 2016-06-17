9d9d80b48447b7ec1b26f502e4bb0be0bb3ac3b6
Andrew Nagy <andrew.nagy@the159.com>
2016-06-17 16:20:18 -0700

    FREEPBX-12570 Cant select inherit music group

--- views/form.php
+++ views/form.php
@@ -212,7 +212,7 @@ if(function_exists('music_list')) {
 							$cur = (isset($ringing) ? $ringing : 'Ring');
 							$tresults = \music_list();
 							$ringhtml .= '<option value="Ring">'._("Ring").'</option>';
-							$ringhtml .= '<option value="inherit">'._("Inherit").'</option>';
+							$ringhtml .= '<option value="inherit" '.($cur == 'inherit' ? ' SELECTED' : '').'>'._("Inherit").'</option>';
 							if (isset($tresults[0])) {
 							foreach ($tresults as $tresult) {
 							    ( $tresult == 'none' ? $ttext = _("none") : $ttext = $tresult );
