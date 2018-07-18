05a3c0ebc730206b7ad7f4c448882e351490764a
James Finstrom <jfinstrom@sangoma.com>
2018-07-18 22:28:54 +0000

    Cleanup restore

--- Restore.php
+++ Restore.php
@@ -3,10 +3,10 @@ namespace FreePBX\modules\Ringgroups;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    foreach ($configd as $rg) {
+    $configs = reset($this->getConfigs());
+    foreach ($configs as $rg) {
         $this->FreePBX->Ringgroups->delete($rg['grpnum'])
-            ->add($rg['grpnum'], $rg['strategy'], $rg['grptime'], $rg['grplist'], $rg['postdest'], $rg['desc'], $rg['grppre'], $rg['annmsg_id'], $rg['alertinfo'], $rg['needsconf'], $rg['remotealert_id'], $rg['toolate_id'], $rg['ringing'], $rg['cwignore'], $rg['cfignore'], $rg['changecid'], $rg['fixedcid'], $rg['cpickup'], $rg['recording'], $rg['progress'], $rg['elsewhere'], $rg['rvolume']);
+            ->add($rg['grpnum'], $rg['strategy'], $rg['grptime'], $rg['grplist'], $rg['postdest'], $rg['description'], $rg['grppre'], $rg['annmsg_id'], $rg['alertinfo'], $rg['needsconf'], $rg['remotealert_id'], $rg['toolate_id'], $rg['ringing'], $rg['cwignore'], $rg['cfignore'], $rg['changecid'], $rg['fixedcid'], $rg['cpickup'], $rg['recording'], $rg['progress'], $rg['elsewhere'], $rg['rvolume']);
     }
   }
 }
