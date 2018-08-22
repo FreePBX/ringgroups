4407ede2191703c0f7403ecbcb871dbc1728dac4
Jason Parker <jparker@sangoma.com>
2018-08-22 10:32:41 +0000

    Fix restore

--- Restore.php
+++ Restore.php
@@ -26,7 +26,6 @@ class Restore Extends Base\RestoreBase{
       $configs[] = $config[0];
     }
     $bmo->resetDatabase();
-    $configs = reset($configs);
     foreach ($configs as $rg) {
       $this->FreePBX->Ringgroups->delete($rg['grpnum'])
         ->add($rg['grpnum'], $rg['strategy'], $rg['grptime'], $rg['grplist'], $rg['postdest'], $rg['description'], $rg['grppre'], $rg['annmsg_id'], $rg['alertinfo'], $rg['needsconf'], $rg['remotealert_id'], $rg['toolate_id'], $rg['ringing'], $rg['cwignore'], $rg['cfignore'], $rg['changecid'], $rg['fixedcid'], $rg['cpickup'], $rg['recording'], $rg['progress'], $rg['elsewhere'], $rg['rvolume']);
