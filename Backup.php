c7e089d70fcc32a24e15326a1ebb2b8a7c5332f2
Andrew Nagy <andrew.nagy@the159.com>
2019-02-11 13:55:05 -0500

    backup and restore

--- Backup.php
+++ Backup.php
@@ -2,16 +2,11 @@
 namespace FreePBX\modules\Ringgroups;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-    $ringroupList = $this->FreePBX->Ringgroups->listRinggroups(true);
-    $configs = [];
-    foreach ($ringroupList as $rg) {
-        $config = $this->FreePBX->Ringgroups->get($rg['grpnum']);
-        $configs[] = $config[0];
-    }
-    $this->addDirectories($dirs);
-    $this->addDependency('core');
-    $this->addDependency('callrecordings');
-    $this->addConfigs($configs);
-  }
+	public function runBackup($id,$transaction){
+		$configs = $this->FreePBX->Database->query('SELECT * FROM ringgroups')->fetchAll(\PDO::FETCH_ASSOC);
+		$this->addDirectories($dirs);
+		$this->addDependency('core');
+		$this->addDependency('callrecordings');
+		$this->addConfigs($configs);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -2,34 +2,78 @@
 namespace FreePBX\modules\Ringgroups;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = reset($this->getConfigs());
-    if (!empty($configs)) {
-        foreach ($configs as $rg) {
-            $this->FreePBX->Ringgroups->delete($rg['grpnum'])
-                ->add($rg['grpnum'], $rg['strategy'], $rg['grptime'], $rg['grplist'], $rg['postdest'], $rg['description'], $rg['grppre'], $rg['annmsg_id'], $rg['alertinfo'], $rg['needsconf'], $rg['remotealert_id'], $rg['toolate_id'], $rg['ringing'], $rg['cwignore'], $rg['cfignore'], $rg['changecid'], $rg['fixedcid'], $rg['cpickup'], $rg['recording'], $rg['progress'], $rg['elsewhere'], $rg['rvolume']);
-        }
-    }
-  } 
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		foreach($configs as $rg) {
+			$this->FreePBX->Ringgroups->delete($rg['grpnum'])
+			->add(
+				$rg['grpnum'],
+				$rg['strategy'],
+				$rg['grptime'],
+				$rg['grplist'],
+				$rg['postdest'],
+				$rg['description'],
+				$rg['grppre'],
+				$rg['annmsg_id'],
+				$rg['alertinfo'],
+				$rg['needsconf'],
+				$rg['remotealert_id'],
+				$rg['toolate_id'],
+				$rg['ringing'],
+				$rg['cwignore'],
+				$rg['cfignore'],
+				$rg['changecid'],
+				$rg['fixedcid'],
+				$rg['cpickup'],
+				$rg['recording'],
+				$rg['progress'],
+				$rg['elsewhere'],
+				$rg['rvolume']
+			);
+		}
+	}
 
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $tables = array_flip($tables+$unknownTables);
-    if(!isset($tables['ringgroups'])){
-      return $this;
-    }
-    $bmo = $this->FreePBX->Ringgroups;
-    $bmo->setDatabase($pdo);
-    $ringroupList = $bmo->listRinggroups(true);
-    $configs = [];
-    foreach ($ringroupList as $rg) {
-      $config = $bmo->get($rg['grpnum']);
-      $configs[] = $config[0];
-    }
-    $bmo->resetDatabase();
-    foreach ($configs as $rg) {
-      $this->FreePBX->Ringgroups->delete($rg['grpnum'])
-        ->add($rg['grpnum'], $rg['strategy'], $rg['grptime'], $rg['grplist'], $rg['postdest'], $rg['description'], $rg['grppre'], $rg['annmsg_id'], $rg['alertinfo'], $rg['needsconf'], $rg['remotealert_id'], $rg['toolate_id'], $rg['ringing'], $rg['cwignore'], $rg['cfignore'], $rg['changecid'], $rg['fixedcid'], $rg['cpickup'], $rg['recording'], $rg['progress'], $rg['elsewhere'], $rg['rvolume']);
-    }
-    return $this;
-  }
+	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
+		$tables = array_flip($tables+$unknownTables);
+		if(!isset($tables['ringgroups'])){
+			return $this;
+		}
+		$bmo = $this->FreePBX->Ringgroups;
+		$bmo->setDatabase($pdo);
+		$ringroupList = $bmo->listRinggroups(true);
+		$configs = [];
+		foreach ($ringroupList as $rg) {
+			$config = $bmo->get($rg['grpnum']);
+			$configs[] = $config[0];
+		}
+		$bmo->resetDatabase();
+		foreach ($configs as $rg) {
+			$this->FreePBX->Ringgroups->delete($rg['grpnum'])
+			->add(
+				$rg['grpnum'],
+				$rg['strategy'],
+				$rg['grptime'],
+				$rg['grplist'],
+				$rg['postdest'],
+				$rg['description'],
+				$rg['grppre'],
+				$rg['annmsg_id'],
+				$rg['alertinfo'],
+				$rg['needsconf'],
+				$rg['remotealert_id'],
+				$rg['toolate_id'],
+				$rg['ringing'],
+				$rg['cwignore'],
+				$rg['cfignore'],
+				$rg['changecid'],
+				$rg['fixedcid'],
+				$rg['cpickup'],
+				$rg['recording'],
+				$rg['progress'],
+				$rg['elsewhere'],
+				$rg['rvolume']
+			);
+		}
+		return $this;
+	}
 }
